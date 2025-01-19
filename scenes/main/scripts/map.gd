7508ba5b4de5ffc3e4d12328992d22772d953b30
Buğra Hasbek <bugrahasbek@gmail.com>
2025-01-19 20:34:30 +0100

    generate symetrical map

--- scenes/main/scripts/map.gd
+++ scenes/main/scripts/map.gd
@@ -2,13 +2,15 @@ extends TileMapLayer
 
 const RIVER_RAID_ATLAS_ID = 0
 const INITIAL_MAP_BOTTOM_INDEX = -1
+const MIN_RIVER_WIDTH = 4
+const EXPANSION_SIZE = 2
 
 const LAND              : Vector2 = Vector2(0, 0)
 const RIVER             : Vector2 = Vector2(1, 0)
 const HOUSE             : Vector2 = Vector2(2, 0)
-const RIVER_NARROW_LEFT : Vector2 = Vector2(3, 0)
+const RIVER_SHRINK_LEFT : Vector2 = Vector2(3, 0)
 const ROAD              : Vector2 = Vector2(0, 1)
-const RIVER_NARROW_RIGHT: Vector2 = Vector2(1, 1)
+const RIVER_SHRINK_RIGHT: Vector2 = Vector2(1, 1)
 const RIVER_EXPAND_LEFT : Vector2 = Vector2(2, 1)
 const RIVER_EXPAND_RIGHT: Vector2 = Vector2(3, 1)
 
@@ -27,7 +29,7 @@ func generate_map(visible_rect_global: Rect2) -> void:
 	self.river_start_x = (self.map_size_x - self.river_width) / 2  # Center river
 	
 	for y in range(map_size_y):
-		add_new_row(y)
+		add_new_row(-y + INITIAL_MAP_BOTTOM_INDEX)
 
 func update_tilemap_frustum(visible_rect_global: Rect2) -> void:
 	# the camera is restricted to move only vertically up
@@ -74,44 +76,63 @@ func is_bottom_row_of_map_invisible(visible_rect_global: Rect2) -> bool:
 	return map_bottom_row_top_y_coordinate > camera_bottom_y_coordinate
 
 func add_new_row(y_index: int) -> void:
-	var river_expanded_left : bool = false
-	var river_narrowed_right: bool = false
-	var river_width_next    : int  = river_width
-	var river_start_x_next  : int  = river_start_x
+	var tile
 	
+	var is_expanding: bool = false
+	var is_shrinking: bool = false
+	
+	var river_width_next = self.river_width
+	var river_start_x_next = self.river_start_x
+	# 15% chance to expand, 15% to shrink, 70% to stay the same
+	if randi_range(1, 100) <= 15 and river_width >= MIN_RIVER_WIDTH + EXPANSION_SIZE:
+		river_width_next -= EXPANSION_SIZE
+		river_start_x_next += EXPANSION_SIZE / 2
+		is_shrinking = true
+	elif randi_range(1, 100) >= 85 and river_width <= map_size_x - (MIN_RIVER_WIDTH / 2) - EXPANSION_SIZE:
+		river_width_next += EXPANSION_SIZE
+		river_start_x_next -= EXPANSION_SIZE / 2
+		is_expanding = true
+	
+	# let's assume river start x was 7 and river is expanding
+	# in this case tile 7 should be river and tile 6 should be half river half land
+	# This is the reason why river start x is updated *before* creating tile in previous river start position
+	if is_expanding:
+		self.river_start_x = river_start_x_next
+		self.river_width = river_width_next
+
 	for x in range(map_size_x):
-		var tile = LAND
-			
-		# Determine if the tile is part of the river or land
-		if x >= river_start_x and x < river_start_x + river_width:
-			if randi_range(0, 10) < 2:  # Small chance for islands in the river
-				tile = Vector2i(0, 0)  # Ground tile (island)
-			else:
-				tile = Vector2i(1, 0)  # River tile
-		else:
+		if x < river_start_x or x > river_start_x + river_width:
 			tile = LAND
-			
-		# Add continuity by adjusting the river width
-		if x == river_start_x - 1 and randi_range(0, 5) == 0:  # Left edge of the river
+		elif x == river_start_x:
+			if is_expanding:
 				tile = RIVER_EXPAND_LEFT
-				river_expanded_left = true
-				river_start_x_next = river_start_x_next - 1
-				river_width_next = river_width_next + 1
-		elif x == river_start_x and randi_range(0, 5) == 0 and not river_expanded_left:  # Narrowing river on the left
-			tile = RIVER_NARROW_LEFT
-			river_start_x_next = river_start_x_next + 1
-			river_width_next = river_width_next - 1
-		elif x == river_start_x + river_width - 1 and randi_range(0, 5) == 0:  # Narrowing river on the right
-			tile =RIVER_NARROW_RIGHT
-			river_narrowed_right = true
-			river_width_next = river_width_next - 1
-		elif x == river_start_x + river_width and randi_range(0, 5) == 0 and not river_narrowed_right:  # Right edge of the river
+				print("expanding left. X:%d" % x)
+			elif is_shrinking:
+				print("shrinking left. X:%d" % x)
+				tile = RIVER_SHRINK_LEFT
+			else:
+				tile = RIVER
+		elif x == river_start_x + river_width:
+			if is_expanding:
 				tile = RIVER_EXPAND_RIGHT
-				river_width_next = river_width_next + 1
+				print("expanding right. X:%d" % x)
+			elif is_shrinking:
+				tile = RIVER_SHRINK_RIGHT
+				print("shrinking right. X:%d" % x)
+			else:
+				tile = RIVER
+		else:
+			tile = RIVER
+		
+		# Set the cell in the TileMapLayer
 		set_cell(Vector2i(x, y_index), RIVER_RAID_ATLAS_ID, tile, 0)
 	
-	self.river_start_x = river_start_x_next
-	self.river_width = river_width_next
+	# let's assume river start x was 7 and river is shrinking
+	# in this case tile 7 should be half river half land
+	# This is the reason why river start x is updated *after* creating tile in previous river start position
+	if is_shrinking:
+		self.river_start_x = river_start_x_next
+		self.river_width = river_width_next
 
 func erase_bottom_row() -> void:
 	var map_size_x = get_used_rect().size.x
